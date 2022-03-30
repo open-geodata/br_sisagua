e009df451854c3fc23fdd96ee753ce53a3ac925b
Michel Metran <michelmetran@gmail.com>
2022-03-30 16:46:24 -0300

    Ajustes

--- src/sisagua/dash_sisagua.py
+++ src/sisagua/dash_sisagua.py
@@ -2,57 +2,31 @@
 # coding: utf-8
 
 import os
-import sys
 import pandas as pd
-from datetime import date
-
-#import folium
-#import numpy as np
-#import seaborn as sns
-#import geopandas as gpd
-#from folium import plugins
 
 
-try:
-    print('Read "ibge" from Python File')
-    #from ibge import *
-    
-    
-    def adjust_cod_ibge(cod_ibge):
-        if len(str(cod_ibge)) == 6:
-            print('Padrão IBGE antigo, com código de 6 dígitos.\nSem correções necessárias.')
-            cod_ibge = int(cod_ibge)
-            print('Código IBGE: {}'.format(cod_ibge))
+def adjust_cod_ibge(cod_ibge):
+    if len(str(cod_ibge)) == 6:
+        print('Padrão IBGE antigo, com código de 6 dígitos.\nSem correções necessárias.')
+        cod_ibge = int(cod_ibge)
+        print('Código IBGE: {}'.format(cod_ibge))
 
-        elif len(str(cod_ibge)) == 7:
-            print('Padrão IBGE novo, com código de 7 dígitos.\nCorreções necessárias aplicadas!')
-            cod_ibge = cod_ibge[0:6]
-            cod_ibge = int(cod_ibge)
-            print('Código IBGE: {}'.format(cod_ibge))
+    elif len(str(cod_ibge)) == 7:
+        print('Padrão IBGE novo, com código de 7 dígitos.\nCorreções necessárias aplicadas!')
+        cod_ibge = cod_ibge[0:6]
+        cod_ibge = int(cod_ibge)
+        print('Código IBGE: {}'.format(cod_ibge))
 
-        else:
-            print('Padrão Diferente!\nDesenvolver correção!')
+    else:
+        print('Padrão Diferente!\nDesenvolver correção!')
 
-        return cod_ibge
-
-
-    
-except:
-    pass
-
-
-try:
-    print('Read "paths" from Jupyter')
-    from paths import *
-    
-except:
-    print('Não deu pra importar paths')
+    return cod_ibge
 
 
 # Parameters
 estado = 'SP'
-cod_ibge = '3548906' # São Carlos
-cod_ibge = '3526902' # Limeira
+cod_ibge = '3548906'  # São Carlos
+cod_ibge = '3526902'  # Limeira
 
 # Adjust Code
 cod_ibge_adjusted = adjust_cod_ibge(cod_ibge)
@@ -61,46 +35,39 @@ cod_ibge_adjusted = adjust_cod_ibge(cod_ibge)
 city_path = '{}_{}'.format(estado, cod_ibge_adjusted)
 city_path
 
-
 try:
     print('Read "data" from Python File')
-    output_path_cidades = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'output', 'cidades'))
+    output_path_cidades = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'output', 'cidades'))
     df = pd.read_excel(
         os.path.join(output_path_cidades, city_path, 'vigilancia', 'vigilancia_parametros_basicos.xlsx')
     )
 except:
     pass
 
-
 # Results
 print(df.info())
-
 
 # Select Parameters
 df = df[df['Parâmetro (Parâmetros Básicos)'].str.contains('Cloro')]
 
-
 # Ajusta Resultados
-df['Resultado'] = df['Resultado'].astype(str).str.replace(',','.')
+df['Resultado'] = df['Resultado'].astype(str).str.replace(',', '.')
 df.loc['Resultado'] = pd.to_numeric(df['Resultado'], errors='coerce')
 df['Resultado']
 df.head()
-
 
 # Adjust Dates
 df['Data Da Coleta'] = pd.to_datetime(df['Data Da Coleta'])
 df['Data Do Laudo'] = pd.to_datetime(df['Data Do Laudo'])
 df['Data De Registro No Sisagua'] = pd.to_datetime(df['Data De Registro No Sisagua'])
 
-
 import dash
 from dash import Dash, dcc, html, Input, Output
 from jupyter_dash import JupyterDash
 
-
 import plotly.express as px
 import plotly.graph_objects as go
-
 
 # Start
 app = JupyterDash(__name__)
@@ -112,7 +79,7 @@ app.layout = html.Div([
             [
                 dcc.Dropdown(
                     df['Nome Da Forma De Abastecimento'].unique(),
-                    #value='Fertility rate, total (births per woman)',
+                    # value='Fertility rate, total (births per woman)',
                     id='xaxis-column'
                 ),
             ],
@@ -122,6 +89,7 @@ app.layout = html.Div([
     dcc.Graph(id='indicator-graphic'),
 ])
 
+
 @app.callback(
     Output('indicator-graphic', 'figure'),
     Input('xaxis-column', 'value'),
@@ -129,12 +97,12 @@ app.layout = html.Div([
 def update_graph(xaxis_column_name):
     # Create Figure
     fig = go.Figure()
-    
+
     # Add trace
     fig.add_trace(
         go.Scatter(
             x=df['Data Da Coleta'],
-            #y=df['Resultado'],
+            # y=df['Resultado'],
             y=df[df['Nome Da Forma De Abastecimento'] == xaxis_column_name]['Resultado'],
             name='conclusao',
             mode='markers',
@@ -142,7 +110,7 @@ def update_graph(xaxis_column_name):
             opacity=0.8,
         )
     )
-    
+
     # Udate
     fig.update_layout(
         title='Cloro Residual na "{}"'.format(xaxis_column_name),
@@ -150,19 +118,16 @@ def update_graph(xaxis_column_name):
         margin={
             'l': 40,
             'b': 40,
-            #'t': 40,
+            # 't': 40,
             'r': 0
         },
-        #dragmode='pan',
+        # dragmode='pan',
         hovermode='x',
 
     )
-    #fig.write_html('ddd.html', config=config)
+    # fig.write_html('ddd.html', config=config)
     return fig
+
 
 # Run
 app.run_server(mode='inline', port=8051)
-
-
-
-
