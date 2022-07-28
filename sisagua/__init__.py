3211ce314a72374bea650d42d1f43047bf8ae47e
Michel Metran <michelmetran@gmail.com>
2022-07-27 21:29:16 -0300

    s

---
+++ sisagua/__init__.py
@@ -0,0 +1,7 @@
+from . import graphs
+from . import ibge
+from . import page_cadastro
+from . import page_controle
+from . import page_vigilancia
+from . import bitio
+from . import basic_parameters
