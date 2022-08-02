b854f231b18670d66e89be172f03df33c3d165df
Michel Metran <michelmetran@gmail.com>
2022-08-02 18:50:43 -0300

    Ajustes

--- sisagua/packages.py
+++ sisagua/packages.py
@@ -4,9 +4,11 @@ sss
 
 import dash
 import pandas as pd
-import dash_bootstrap_components as dbc
 
 from dash import Dash, Input, Output, dcc, html
+
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 from dash_bootstrap_templates import ThemeSwitchAIO
 
 import plotly.graph_objects as go
@@ -15,5 +17,3 @@ from open_geodata import geo
 
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
-
-from dash_bootstrap_templates import load_figure_template
