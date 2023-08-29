1b7921c209caa93afe7affe234d88dacea7c384b
Michel Metran <michelmetran@gmail.com>
2023-08-29 19:50:10 -0300

    Ajustes

--- scripts/paths.py
+++ scripts/paths.py
@@ -1,8 +1,9 @@
 """
-sss
+Pastas do Projeto
+julho de 2023
 """
 
-import os
+
 from pathlib import Path
 
 
@@ -16,37 +17,45 @@ data_path.mkdir(exist_ok=True)
 docs_path = project_path / 'docs'
 docs_path.mkdir(exist_ok=True)
 
-bruto_path = os.path.join(data_path, 'brutos')
-input_path = os.path.join(data_path, 'input')
-output_path = os.path.join(data_path, 'output')
+# Bruto
+bruto_path = data_path / 'brutos'
+bruto_path.mkdir(exist_ok=True)
 
 # Input
-input_path_parquet = os.path.join(input_path, 'parquet')
-input_path_parquet_partitioned = os.path.join(input_path, 'parquet_partitioned')
+input_path = data_path / 'input'
+input_path.mkdir(exist_ok=True)
+
+input_path_parquet = input_path / 'parquet'
+input_path_parquet.mkdir(exist_ok=True)
+
+input_path_parquet_partitioned = input_path / 'parquet_partitioned'
+input_path_parquet_partitioned.mkdir(exist_ok=True)
+
+output_path = data_path / 'output'
+output_path.mkdir(exist_ok=True)
+
 
 # Bruto
+# import os
 # output_path_cidades = os.path.join(output_path, 'cidades')
 
 # Cria
-os.makedirs(bruto_path, exist_ok=True)
-os.makedirs(input_path, exist_ok=True)
-os.makedirs(output_path, exist_ok=True)
+# os.makedirs(bruto_path, exist_ok=True)
+# os.makedirs(input_path, exist_ok=True)
+# os.makedirs(output_path, exist_ok=True)
 
 # Inputs
-os.makedirs(input_path_parquet, exist_ok=True)
-os.makedirs(input_path_parquet_partitioned, exist_ok=True)
+# os.makedirs(input_path_parquet, exist_ok=True)
+# os.makedirs(input_path_parquet_partitioned, exist_ok=True)
 
 # Cria
 # os.makedirs(output_path_cidades, exist_ok=True)
-
 
 # controle_path = os.path.join(bruto_path, 'controle')
 # vigilancia_path = os.path.join(bruto_path, 'vigilancia')
 # os.makedirs(cadastro_path, exist_ok=True)
 # os.makedirs(controle_path, exist_ok=True)
 # os.makedirs(vigilancia_path, exist_ok=True)
-
-#
 #
 # project_path = Path(__file__).parents[1].resolve()
 #
